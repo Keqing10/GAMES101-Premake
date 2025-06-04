0d0a2dfbb9bcdafe37348be48108b25f0d80bc0f
Mars <marsptt@outlook.com>
2025-06-04 21:46:28 +0800

    hw0,1,2

--- Frame/Source/Assignment/Assignment0/main.cpp
+++ Frame/Source/Assignment/Assignment0/main.cpp
@@ -1,48 +1,68 @@
-#include<cmath>
+﻿#include<cmath>
 #include<eigen3/Eigen/Core>
 #include<eigen3/Eigen/Dense>
 #include<iostream>
 
-int main(){
+constexpr float PI = EIGEN_PI;
 
-    // Basic Example of cpp
-    std::cout << "Example of cpp \n";
-    float a = 1.0, b = 2.0;
-    std::cout << a << std::endl;
-    std::cout << a/b << std::endl;
-    std::cout << std::sqrt(b) << std::endl;
-    std::cout << std::acos(-1) << std::endl;
-    std::cout << std::sin(30.0/180.0*acos(-1)) << std::endl;
+int main() {
 
-    // Example of vector
-    std::cout << "Example of vector \n";
-    // vector definition
-    Eigen::Vector3f v(1.0f,2.0f,3.0f);
-    Eigen::Vector3f w(1.0f,0.0f,0.0f);
-    // vector output
-    std::cout << "Example of output \n";
-    std::cout << v << std::endl;
-    // vector add
-    std::cout << "Example of add \n";
-    std::cout << v + w << std::endl;
-    // vector scalar multiply
-    std::cout << "Example of scalar multiply \n";
-    std::cout << v * 3.0f << std::endl;
-    std::cout << 2.0f * v << std::endl;
+	// Basic Example of cpp
+	std::cout << "Example of cpp \n";
+	float a = 1.0, b = 2.0;
+	std::cout << a << std::endl;
+	std::cout << a / b << std::endl;
+	std::cout << std::sqrt(b) << std::endl;
+	std::cout << std::acos(-1) << std::endl;
+	std::cout << std::sin(30.0 / 180.0 * acos(-1)) << std::endl;
 
-    // Example of matrix
-    std::cout << "Example of matrix \n";
-    // matrix definition
-    Eigen::Matrix3f i,j;
-    i << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
-    j << 2.0, 3.0, 1.0, 4.0, 6.0, 5.0, 9.0, 7.0, 8.0;
-    // matrix output
-    std::cout << "Example of output \n";
-    std::cout << i << std::endl;
-    // matrix add i + j
-    // matrix scalar multiply i * 2.0
-    // matrix multiply i * j
-    // matrix multiply vector i * v
+	// Example of vector
+	std::cout << "Example of vector \n";
+	// vector definition
+	Eigen::Vector3f v(1.0f, 2.0f, 3.0f);
+	Eigen::Vector3f w(1.0f, 0.0f, 0.0f);
+	// vector output
+	std::cout << "Example of output \n";
+	std::cout << v << std::endl;
+	// vector add
+	std::cout << "Example of add \n";
+	std::cout << v + w << std::endl;
+	// vector scalar multiply
+	std::cout << "Example of scalar multiply \n";
+	std::cout << v * 3.0f << std::endl;
+	std::cout << 2.0f * v << std::endl;
 
-    return 0;
+	// Example of matrix
+	std::cout << "Example of matrix \n";
+	// matrix definition
+	Eigen::Matrix3f i, j;
+	i << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
+	j << 2.0, 3.0, 1.0, 4.0, 6.0, 5.0, 9.0, 7.0, 8.0;
+	// matrix output
+	std::cout << "Example of output \n";
+	std::cout << i << std::endl;
+	// matrix add i + j
+	// matrix scalar multiply i * 2.0
+	// matrix multiply i * j
+	// matrix multiply vector i * v
+	std::cout << "Example of add \n";
+	std::cout << i + j << std::endl;
+	std::cout << "Example of scalar multiply \n";
+	std::cout << i * 2.0 << std::endl;
+	std::cout << "Example of multiply \n";
+	std::cout << i * j << std::endl;
+	std::cout << "Example of multiply vector \n";
+	std::cout << i * v << std::endl;
+
+	// pa0
+	std::cout << "\npa0:\n";
+	Eigen::Vector3f p(2.0f, 1.0f, 1);
+	Eigen::Matrix3f R({
+		{ cos(PI / 4), -sin(PI / 4), 0 },
+		{ sin(PI / 4), cos(PI / 4), 0 },
+		{ 0, 0, 1 } });
+	Eigen::Matrix3f T({ {1, 0, 1}, {0, 1, 2}, {0, 0, 1} });
+	std::cout << T * R * p << std::endl;
+
+	return 0;
 }
